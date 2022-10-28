2717482f5c1c2eba37a3d9f11d6f0d9ce0c135e8
Laurie Roy <laurieroy.dev@gmail.com>
2022-10-28 15:37:53 -0700

    Add ListingItem component, my listing section to profile

---
+++ src/pages/ListingItem.jsx
@@ -0,0 +1,6 @@
+
+export const ListingItem = ({listing, id}) => {
+	return (
+		<div>{listing.name}</div>
+	)
+}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
-import { getAuth, updateProfile } from "firebase/auth";
-import { doc, updateDoc } from "firebase/firestore";
-import { useNavigate } from "react-router";
-import { FcHome } from "react-icons/fc";
-import { toast } from "react-toastify";
-import { db } from "../firebase";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { getAuth, updateProfile } from 'firebase/auth';
+import {
+  collection,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
+import { useNavigate } from 'react-router';
+import { FcHome } from 'react-icons/fc';
+import { toast } from 'react-toastify';
+import { db } from '../firebase';
+import { Link } from 'react-router-dom';
+import { ListingItem } from './ListingItem';
 
 export default function Profile() {
   const auth = getAuth();
@@ -14,7 +23,8 @@ export default function Profile() {
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
-
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
   const { name, email } = formData;
   const navigate = useNavigate();
 
@@ -27,7 +37,7 @@ export default function Profile() {
 
   function onLogout() {
     auth.signOut();
-    navigate("/");
+    navigate('/');
   }
 
   async function onSubmit() {
@@ -38,61 +48,83 @@ export default function Profile() {
           displayName: name,
         });
 
-        const docRef = doc(db, "users", auth.currentUser.uid);
+        const docRef = doc(db, 'users', auth.currentUser.uid);
         await updateDoc(docRef, {
           name,
         });
       }
-      toast.success("Profile details updated");
+      toast.success('Profile details updated');
     } catch (error) {
-      toast.error("Could not update profile details");
+      toast.error('Could not update profile details');
     }
   }
 
+  useEffect(() => {
+    async function fetchUserListings() {
+      const listingRef = collection(db, 'listings');
+      const q = query(
+        listingRef,
+        where('userRef', '==', auth.currentUser.uid),
+        orderBy('timestamp', 'desc')
+      );
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+      setLoading(false);
+    }
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
+
   return (
     <>
-      <section className="max-w-6xl mx-auto flex flex-col justify-center items-center">
-        <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
-        <div className="w-full md:w-[50%] mt-6 px-3">
+      <section className='max-w-6xl mx-auto flex flex-col justify-center items-center'>
+        <h1 className='text-3xl text-center mt-6 font-bold'>My Profile</h1>
+        <div className='w-full md:w-[50%] mt-6 px-3'>
           <form>
             {/* Name input */}
             <input
               disabled={!changeDetails}
               onChange={onChange}
-              type="text"
-              name="name"
-              id="name"
+              type='text'
+              name='name'
+              id='name'
               value={name}
               className={`w-full px-4 py-2 text-xl mb-6 text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
-                changeDetails && "bg-red-200 focus:bg-red-200"
+                changeDetails && 'bg-red-200 focus:bg-red-200'
               }`}
             />
             {/* Email input */}
             <input
               disabled={!changeDetails}
               onChange={onChange}
-              type="email"
-              name="email"
-              id="email"
+              type='email'
+              name='email'
+              id='email'
               value={email}
-              className="w-full px-4 py-2 text-xl mb-6 text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+              className='w-full px-4 py-2 text-xl mb-6 text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out'
             />
 
-            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
-              <p className="flex items-center mb-6">
+            <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
+              <p className='flex items-center mb-6'>
                 Do you want to change your name?
                 <span
-                  className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1 cursor-pointer"
+                  className='text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1 cursor-pointer'
                   onClick={() => {
                     changeDetails && onSubmit();
                     setChangeDetails((prevState) => !prevState);
                   }}
                 >
-                  {changeDetails ? "Apply change" : "Edit"}
+                  {changeDetails ? 'Apply change' : 'Edit'}
                 </span>
               </p>
               <p
-                className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer"
+                className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer'
                 onClick={onLogout}
               >
                 Sign out
@@ -100,19 +132,35 @@ export default function Profile() {
             </div>
           </form>
           <button
-            type="submit"
-            className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:shadow-lg hover:bg-blue-700 active:bg-blue-800 transition  duration-150 ease-in-out"
+            type='submit'
+            className='w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:shadow-lg hover:bg-blue-700 active:bg-blue-800 transition  duration-150 ease-in-out'
           >
             <Link
-              to="/create-listing"
-              className="flex justify-center items-center"
+              to='/create-listing'
+              className='flex justify-center items-center'
             >
-              <FcHome className="text-3xl bg-red-200 rounded-full p-1 border-2 mr-2" />
+              <FcHome className='text-3xl bg-red-200 rounded-full p-1 border-2 mr-2' />
               Sell or rent your home
             </Link>
           </button>
         </div>
       </section>
+      <div className='max-w-6xl px-3 mt-6 mx-auto'>
+        {!loading && listings.length > 0 && (
+          <>
+            <h2 className='text-2xl text-center font-semibold'>My Listings</h2>
+            <ul>
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                />
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
     </>
   );
 }
